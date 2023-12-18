80fc94b7c1f02edcb826a3e764ceb052c058bcf8
radhikesh <radhikesh310@gmail.com>
2023-12-18 11:22:11 +0530

    update addition and substraction

---
+++ addition/add.py
@@ -0,0 +1,10 @@
+def add(a, b):
+    return a + b
+
+# Example usage for addition:
+num1_add = float(input("Enter the first number for addition: "))
+num2_add = float(input("Enter the second number for addition: "))
+
+sum_result = add(num1_add, num2_add)
+
+print(f"Sum: {sum_result}")
